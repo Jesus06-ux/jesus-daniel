e5aae677daf92fb0c3abeca4ea2b58efaa593526
jesus <dc17065@gmail.com>
2023-01-26 21:45:57 -0500

    daniel

--- python/descuento.py
+++ python/descuento.py
@@ -5,17 +5,26 @@
 # 	2. Si el precio es mayor a $100 y menor a $200 el 20%
 # 	3. Si el precio es mayor a $200 el 25%
 
-# Es su deber como desarrollador escribir un algoritmo que le permita al comerciante 
+# Es su deber como desarrollador escribir un algoritmo que le permita al comerciante
 # ingresar la cantidad de productos y el precio de cada uno. El algoritmo
 # tambien debe imprimir en pantalla el calculo de los descuentos.
 
-producto = int(input("ingrese la cantidad de productos: "))
-descuento = 0
-valortotal = 0
 
+producto = int(input("ingrese la cantidad de productos: "))
 
 while producto <= 0:
-    producto=int(input("ingrese el prodocto"))
-
-for i in range(0, producto):   
-    print("rfgtyhgb") 
+    producto = int(input("Error: ingrese el prodocto de nuevo: "))
+for i in range(0, producto):
+    valor = int(input("ingrese el valor del producto: "))
+    if valor >= 100:
+       descuento = valor*0.10
+       valortotal = valor - descuento
+       print("el valor total es: ", valortotal)
+    elif valor <= 200:
+       descuento = valor*0.20
+       valortotal = valor - descuento
+       print("el valor total es: ", valortotal)
+    elif valor > 200:
+       descuento = valor*0.25
+       valortotal = valor - descuento
+       print("el valor total es: ", valortotal)
